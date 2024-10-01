c145bbd5eeb12df7ebe144a69845bb79c8ac8a2d
*abrahamnyuiadzi* <abrahamnyuiadzi>
2024-10-01 16:24:53 +0000

    push

--- controllers/users.js
+++ controllers/users.js
@@ -3,9 +3,9 @@ const objectId = require('mongodb').ObjectId;
 
 const getAll =async(req,res)=>{
     const result =await mongodb.getDatabase().db().collection('users').find();
-    result.toArray().then((contacts)=>{
+    result.toArray().then((users)=>{
         res.setHeader('Content-Type','application/json');
-        res.status(200).json(contacts);
+        res.status(200).json(users);
 
 
     });
@@ -18,7 +18,7 @@ const getSingle =async(req,res)=>{
     const result =await mongodb.getDatabase().db().collection('users').find({_id:userId});
     result.toArray().then((contacts)=>{
         res.setHeader('Content-Type','application/json');
-        res.status(200).json(contacts[0]);
+        res.status(200).json(users[0]);
      });
     
 }
