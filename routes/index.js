11bd1f7751ec20339f580fc3c55c9e527ca6fea7
*abrahamnyuiadzi* <abrahamnyuiadzi>
2024-10-09 08:50:54 +0000

    passport

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,28 @@
-const router = require('express').Router();
+
+const { passport } = require('passport');
+const router = require("express").Router();
+
 
 
 router.use('/' , require('./swagger'))
+router.use('/users', require('./users.js'));
 router.get('/', (req,res)=>{
   
     res.send('hello word')
 });
 
-router.use('/users', require('./users.js'));
+
+
+router.get('/login', passport.authenticate('github'),(req,res)=>{});
+
+router.get('/logout',function (req,res, next){
+    req.logOut(function(err){
+        if (err){return next(err);}
+        res.redirect('/')
+    });
+
+
+});
+
 
 module.exports = router ;
